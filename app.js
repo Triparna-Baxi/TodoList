7649559e2f8159d70e2cfc9ef92de108a2e30137
Triparna-Baxi <triparna.baxi15@gmail.com>
2023-06-03 15:38:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,139 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const _= require("lodash");
+
+const app=express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect(("your database url"), {useNewYrlParser:true});
+const itemsSchema ={
+    name: String,
+};
+
+const Item= mongoose.model("Item",itemsSchema);
+
+const item1=new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2=new Item({
+    name: "Hit + button to add new item."
+});
+
+const item3=new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems= [item1, item2,item3];
+
+const listSchema={
+    name: String,
+    items: [itemsSchema]
+};
+
+const List=mongoose.model("List", listSchema);
+
+app.get("/",function(req,res){
+    
+
+
+    Item.find({}, function(err, foundItems){
+        
+        if(foundItems.length ===0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("Successfully saved default items to DB.");
+                }
+            });
+            res.redirect("/");
+        }else{
+            res.render("list",{listTitle: "Today",newListItems: foundItems });
+        }
+    });    
+});
+
+app.get("/:customListName", function(req,res){
+
+   const customListName=_.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName}, function(err, foundList){
+        if(!err){
+            if(!foundList){
+                //New List
+                const list= new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" +customListName);
+            }
+            else{
+                //Existing List
+                res.render("list", {listTitle: foundList.name ,newListItems: foundList.items });
+            }
+        }
+    });
+
+
+   list.save();
+
+});
+
+app.post("/", function(req,res){
+
+    const itemName=req.body.newItem;
+    const listName=req.body.list;
+
+    const item=new Item({
+        name: itemName
+    });
+
+    if(listName === "Today"){
+        item.save();
+        res.redirect("/");
+    }else{
+        List.findOne({name: listName},function(err, foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/"+listName);
+        })
+    }
+});
+
+app.post("/delete", function(req,res){
+    const checkedItemid=req.body.checkbox;
+    const listName=req.body.listName;
+
+    if(listName==="Today"){
+        Item.findByIdAndRemove(checkedItemid, function(err){
+            if(!err){
+                console.log("Successfully deleted checked item");
+                res.redirect("/");
+            }
+        });
+    }else{
+        List.findByIdAndUpdate({name: listName},{$pull:{items: {_id: checkedItemid}}}, function(err, foundList){
+            if(!err){
+                res.redirect("/"+listName);
+            }
+        });
+    }
+
+    
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle:"Work List", newListItems: workItems});
+});
+
+app.listen(3000,function(){
+    console.log("Server running on port 3000");
+}); 
